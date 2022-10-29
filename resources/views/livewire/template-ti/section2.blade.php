82bf67eb8a1a959fd3cfb878e658c4fe18320bc2
mahacreative <gunturmadjid.3@gmail.com>
2022-10-29 14:13:45 +0530

    update

--- resources/views/livewire/template-ti/section2.blade.php
+++ resources/views/livewire/template-ti/section2.blade.php
@@ -73,7 +73,7 @@
                 class="text-white w-[85%] md:w-1/2 text-center italic font-mono text-lg font-light">Dengan memohon Ridho
                 dan Rahmat Allah SWT, kami bermaksud menyelenggarakan akad nikah putra putri kami </p>
         </div>
-        <div class="flex flex-col md:flex-row justify-center items-center my-16 relative gap-y-5 md:gap-x-16">
+        <div class="flex flex-col md:flex-row justify-center items-center my-16 relative gap-y-8 md:gap-x-16">
             <div data-aos="fade-down" data-aos-delay="1000" data-aos-duration="1000" data-aos-easing="ease-out-cubic"
                 class="flex flex-col items-center justify-center gap-y-3 ">
                 <img loading="lazy"
@@ -86,7 +86,7 @@
                 class="h-24 w-24 rounded-full hover:scale-110 flex items-center justify-center">
                 <p class="text-red-600">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-12 md:w-24 h-12 md:h-24">
+                        stroke="currentColor" class="w-24 h-24">
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                     </svg>
